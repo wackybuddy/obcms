a367bc32843ec928ae057bbca1515447b65879c0
morotech <bmsaimambayao@gmail.com>
2025-10-20 13:56:12 +0800

    fixes

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,17 +1,6 @@
 """Test package initialisation hacks for shared fixtures."""
 
-import importlib
-import sys
-import time as _time_module
-
-_PERFORMANCE_MODULE = "tests.test_calendar_performance"
-
-if _PERFORMANCE_MODULE in sys.modules:
-    sys.modules[_PERFORMANCE_MODULE].time = _time_module
-else:
-    module = importlib.import_module(
-        f".{_PERFORMANCE_MODULE.split('.', 1)[1]}", package=__name__
-    )
-    module.time = _time_module
+# Commented out performance module import due to module-level skip causing initialization issues
+# The test_calendar_performance.py module is skipped and doesn't need time module injection
 
 __all__ = []
